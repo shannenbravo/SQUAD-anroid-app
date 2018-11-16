17a991143c218747599faddcfc0f08f45aea4ff3
shannenbravo <shannenbravo@gmail.com>
2018-11-16 15:04:00 -0500

    Revert "Shannen branch"

--- app/src/main/java/project1/mobile/cs/fsu/edu/squad/GridMenu.java
+++ app/src/main/java/project1/mobile/cs/fsu/edu/squad/GridMenu.java
@@ -1,27 +1,13 @@
 package project1.mobile.cs.fsu.edu.squad;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.os.Bundle;
 
 public class GridMenu extends AppCompatActivity {
-//
-    private Intent intent;
-    private LinearLayout myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_menu);
-        intent = new Intent(this,MapsActivity.class);
-        myLocation = (LinearLayout) findViewById(R.id.myLocation);
-        myLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent);
-            }
-        });
     }
 }
