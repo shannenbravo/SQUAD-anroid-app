60b7911f737e11309cba4d53e34d2de267ba2a20
Khoa <kbl13b@my.fsu.edu>
2018-11-17 17:04:58 -0500

    Added Firebase Authenticator
    Added to onClick login. If login is successful goes to GridMenu.

--- app/src/main/java/project1/mobile/cs/fsu/edu/squad/MainActivity.java
+++ app/src/main/java/project1/mobile/cs/fsu/edu/squad/MainActivity.java
@@ -2,34 +2,75 @@ package project1.mobile.cs.fsu.edu.squad;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
     Button loginbutt;
     Button reggi;
+    private FirebaseAuth mAuth;
+    EditText mEmail;
+    EditText mPassword;
+    String email;
+    String password;
+    private String TAG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmail = (EditText) findViewById(R.id.editEmail);
+        mPassword = (EditText) findViewById(R.id.editPassword);
+
+        email = mEmail.getText().toString();
+        password = mPassword.getText().toString();
+
+
         loginbutt = (Button)findViewById(R.id.loginButton);
         reggi = (Button)findViewById(R.id.reggi);
         loginbutt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intent = new Intent(MainActivity.this,GridMenu.class);
-                startActivity(intent);
-            }
-        });
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    intent = new Intent(MainActivity.this, GridMenu.class);
+                                    startActivity(intent);
+                                } else {
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
-        reggi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent = new Intent(MainActivity.this,RegisterUser.class);
-                startActivity(intent);
+                reggi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        intent = new Intent(MainActivity.this, RegisterUser.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
     }
