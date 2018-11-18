12046d1152a6de6ff191a7c3f2057378f5651b8d
khoa <kbl13b@my.fsu.edu>
2018-11-18 15:51:26 -0500

    Test
    test

--- app/src/main/java/project1/mobile/cs/fsu/edu/squad/MainActivity.java
+++ app/src/main/java/project1/mobile/cs/fsu/edu/squad/MainActivity.java
@@ -6,14 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
     Button loginbutt;
+    //private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();*/
+
+        /*mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(MainActivity.this, this /* OnConnectionFailedListener )
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();*/
+
         intent = new Intent(this,GridMenu.class);
         loginbutt = (Button)findViewById(R.id.loginButton);
         loginbutt.setOnClickListener(new View.OnClickListener() {
